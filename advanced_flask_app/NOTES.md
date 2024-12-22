b50e07072805f412566f53b7c736a11d21aa78dc
Wayne McRae <waynem567@gmail.com>
2024-12-22 08:07:05 +0200

    Add Advanced Flask App

---
+++ advanced_flask_app/NOTES.md
@@ -0,0 +1,50 @@
+# Motorcycle website
+
+My final CS50X projest built using:
+
+- HTML
+- CSS
+- JavaScript
+- Flask
+- MySQL
+
+Flask is a lightweight Python framework for web applications.
+
+I'm going to rebuild it here (with improvements) and deploy it to Hostinger.
+
+## This was the original README
+
+Final project for CS50 2023
+MOTORCYCLE website showcasing exclusive motorcycles
+
+1 x Homepage 4 x Product pages 4 x Staff pages 1 x Newsletter/Contact/Go to blog page 1 x Register/Signup page 1 x Blog page 1 x Interact with blogs page
+
+Newsletter to check if email address is already subscribed.
+
+Register to allow user to register with an user name and email address.
+
+Sign In to get user name, email address, and (hashed) password and validate them against existing database
+
+How I got here:
+
+This is the final project for my Harvard CS50 course. I had originally planned on using the homepage I designed for problem set 8, adding database functionality.
+
+Didn't like the basic look of that project for this one so I changed things.
+
+Followed Kevin Powell on Scrimba coding this Frontend Mentor challenge and used that as a template:
+
+https://www.frontendmentor.io/challenges/space-tourism-multipage-website-gRWj1URZ3
+
+I chose this as a template because I had built the original Space Tourism challenge copying the Figma design following along with this video, and I had appreciated the functionality as well as the accessibility options for assisted technologies.
+
+Added Flask functionality for routing and connecting to MySQL database
+
+These are the tables I created in the users.db database:
+
+CREATE TABLE contact ( id INTEGER PRIMARY KEY AUTOINCREMENT, contact_name TEXT NOT NULL, contact_email TEXT NOT NULL, contact_message TEXT NOT NULL );
+
+CREATE TABLE newsletter ( id INTEGER PRIMARY KEY AUTOINCREMENT, newsletter_email TEXT NOT NULL );
+
+CREATE TABLE user ( id INTEGER PRIMARY KEY AUTOINCREMENT, user_name TEXT NOT NULL, password_hash TEXT NOT NULL );
+
+CREATE TABLE blog ( id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP, blog_title TEXT NOT NULL, blog_user TEXT NOT NULL, content TEXT NOT NULL, replies INTEGER NOT NULL, likes INTEGER NOT NULL );
---
+++ advanced_flask_app/static/js/index.js
@@ -0,0 +1,106 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+
+  const visiblity = nav.getAttribute("data-visible");
+  if (visiblity === "false") {
+    nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
+
+// CODE BELOW IS NOT USED IN THIS APPLICATION
+// to use bikes.html instead of seperate pages for each (via app.py)
+// Tabs
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+
+// Keyboard navigation
+// change focus on key input by calling function
+tabList.addEventListener('keydown', changeTabFocus);
+
+// allow select
+// loop through each tab
+tabs.forEach((tab) => {
+  // listen for click and call function
+  tab.addEventListener('click', changeTabPanel);
+});
+
+let tabFocus = 0;
+function changeTabFocus(e) {
+  const keydownLeft = 37;
+  const keydownRight = 39;
+  
+  // change the tabindex of the current tab to -1
+  if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+    tabs[tabFocus].setAttribute("tabindex", -1);
+
+    // if the right key is pushed, move to the next tab on the right
+    if (e.keyCode === keydownRight) {
+      tabFocus++;
+      // ensure keyboard scrolling does not go out of bounds
+      if (tabFocus >= tabs.length) {
+        tabFocus = 0;
+      }
+    // if the left key is pushed, move to the next tab on the left
+    } else {
+      tabFocus--;
+      // ensure keyboard scrolling does not go out of bounds
+      if (tabFocus < 0) {
+        tabFocus = tabs.length - 1;
+      }
+    }
+
+    // set selected tabindex to 0
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    // set the focus on that tab
+    tabs[tabFocus].focus();      
+  }
+};
+
+
+function changeTabPanel(e) {
+  // select target
+  const targetTab = e.target;
+  // get attribute to select individually
+  const targetPanel = targetTab.getAttribute("aria-controls");
+  // get image attribute
+  const targetImage = targetTab.getAttribute("data-image");
+
+  // identify parent and select
+  const tabContainer = targetTab.parentNode;
+  // identify parent above that and select
+  const mainContainer = tabContainer.parentNode;
+
+  // highlighting the active tab
+  tabContainer
+    .querySelector('[aria-selected="true"]')
+    .setAttribute("aria-selected", false);
+
+  targetTab.setAttribute("aria-selected", true);
+
+  // call function to hide all data before new (selected) data is displayed
+  hideContent(mainContainer, '[role="tabpanel"]');
+  // call function to remove hidden attribute from selected data (will select all siblings of that) in order to display that data
+  showContent(mainContainer, [`#${targetPanel}`]);
+
+  // call function to hide images
+  hideContent(mainContainer, 'picture');
+  // call function to remove hidden attribute from selected image in order to display that image
+  showContent(mainContainer, [`#${targetImage}`])
+}
+
+function hideContent(parent, content) {
+  parent
+    .querySelectorAll(content)
+    .forEach((item) => item.setAttribute("hidden", true));
+}
+
+function showContent(parent, content) {
+  parent.querySelector(content).removeAttribute('hidden');
+}
