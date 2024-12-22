1a22aa03b6504d481a8c67f8af2c4c8524246a20
Wayne McRae <waynem567@gmail.com>
2024-12-22 14:00:19 +0200

    Update main.py

--- games/galaxy-fighters/main.py
+++ games/galaxy-fighters/main.py
@@ -2,3 +2,11 @@ import pygame
 
 WIDTH. HEIGHT = 800, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main():
+    run = True
+    
+    while run:
+
+if __name__ == "__main__":
+    main()
