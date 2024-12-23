1ad0c2c767a2d1e6c93306872d8fe283afd4ac68
Wayne McRae <waynem567@gmail.com>
2024-12-23 16:55:38 +0200

    Update main.py

--- games/galaxy-fighters/main.py
+++ games/galaxy-fighters/main.py
@@ -69,10 +69,10 @@ def main():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LCTRL:
-                    bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height//2, 10, 5)
+                    bullet = pygame.Rect(yellow.x + yellow.width, yellow.y + yellow.height//2 - 2, 10, 5)
                     yellow_bullets.append(bullet)
                 if event.key == pygame.K_RCTRL:
-                    bullet = pygame.Rect(red.x, red.y + red.height//2, 10, 5)
+                    bullet = pygame.Rect(red.x, red.y + red.height//2 - 2, 10, 5)
                     red_bullets.append(bullet)
 
         keys_pressed = pygame.key.get_pressed()
