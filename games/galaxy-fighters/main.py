77d401f2a84d506d92bc48bdb08852416a12c6a1
Wayne McRae <waynem567@gmail.com>
2024-12-23 07:27:36 +0200

    Update main.py

--- games/galaxy-fighters/main.py
+++ games/galaxy-fighters/main.py
@@ -20,13 +20,16 @@ RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
     RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 
-def draw_window():
+def draw_window(yellow, red):
     WIN.fill(WHITE)
-    WIN.blit(YELLOW_SPACESHIP, (300, 100))
-    WIN.blit(RED_SPACESHIP, (700, 100))
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
     pygame.display.update()
 
 def main():
+    yellow = pygame.Rect(300, 100, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    red = pygame.Rect(700, 100, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
     clock = pygame.time.Clock()
     run = True
     
