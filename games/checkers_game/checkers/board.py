ee3ca58fe6358d9a8d187afbce492ddfe84a9ca2
Wayne McRae <waynem567@gmail.com>
2024-12-25 15:34:38 +0200

    Update board.py

--- games/checkers_game/checkers/board.py
+++ games/checkers_game/checkers/board.py
@@ -4,3 +4,5 @@ class Board:
     def __init__(self):
         self.board = []
         self.selected_piece = None
+        self.red_left = self.white_left = 12
+        self.red_kings = self.white_kings = 0
