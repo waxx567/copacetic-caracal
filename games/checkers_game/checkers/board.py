ba2408bcae2992b86a2a708f5f276b9bebc67f22
Wayne McRae <waynem567@gmail.com>
2024-12-25 15:50:59 +0200

    Update board.py

--- games/checkers_game/checkers/board.py
+++ games/checkers_game/checkers/board.py
@@ -10,3 +10,6 @@ class Board:
 
     def draw_squares(self, win):
         win.fill(BLACK)
+        for row in range(ROWS):
+            for col in range(row % 2, ROWS, 2):
+                pygame.draw.rect(win, WHITE, (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
