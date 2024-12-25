832eea2e4a72bd67335f7c0e6f0e18b4ff478385
Wayne McRae <waynem567@gmail.com>
2024-12-25 14:39:49 +0200

    Update main.py

--- games/checkers_game/main.py
+++ games/checkers_game/main.py
@@ -3,3 +3,14 @@ from checkers.constants import WIDTH, HEIGHT
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+        pass
+
+main()
