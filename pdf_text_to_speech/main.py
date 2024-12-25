895bfdf3b0c950125c70e1715b0aafbad4e108b0
Wayne McRae <waynem567@gmail.com>
2024-12-25 09:31:47 +0200

    Update main.py

--- pdf_text_to_speech/main.py
+++ pdf_text_to_speech/main.py
@@ -1,6 +1,8 @@
 import pyttsx3
 import PyPDF2
 
+myFile = open('On-the-Origin-of-Species.pdf', 'rb')
+
 speaker = pyttsx3.init()
 
 speaker.say("Hello everyone, welcome to the create audiobook app")
