2408d53bb02ef931ab30b9596fca126d6716132e
Wayne McRae <waynem567@gmail.com>
2024-12-25 09:34:33 +0200

    Update main.py

--- pdf_text_to_speech/main.py
+++ pdf_text_to_speech/main.py
@@ -4,6 +4,7 @@ import PyPDF2
 myFile = open('On-the-Origin-of-Species.pdf', 'rb')
 myReader = PyPDF2.PdfFileReader(myFile)
 numPages = len(myReader.pages)
+print("Number of pages in the PDF: ", numPages)
 
 speaker = pyttsx3.init()
 
