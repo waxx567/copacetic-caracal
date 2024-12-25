fd6f857047a57669c28233484e28ebe4387f0b0e
Wayne McRae <waynem567@gmail.com>
2024-12-25 15:44:55 +0200

    Update board.py

--- games/checkers_game/checkers/board.py
+++ games/checkers_game/checkers/board.py
@@ -1,4 +1,5 @@
 import pygame
+from .constants import RED, WHITE, BLACK, SQUARE_SIZE, ROWS, COLS
 
 class Board:
     def __init__(self):
@@ -6,3 +7,6 @@ class Board:
         self.selected_piece = None
         self.red_left = self.white_left = 12
         self.red_kings = self.white_kings = 0
+
+    def draw_squares(self, win):
+        win.fill(BLACK)
