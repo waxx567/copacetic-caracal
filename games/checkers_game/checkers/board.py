aa366254a66f5d77565ae433b2a88b03bd8055d7
Wayne McRae <waynem567@gmail.com>
2024-12-25 15:28:52 +0200

    Update board.py

--- games/checkers_game/checkers/board.py
+++ games/checkers_game/checkers/board.py
@@ -2,5 +2,5 @@ import pygame
 
 class Board:
     def __init__(self):
-        self.board = [[WHITE, 0, WHITE, 0, WHITE],
-                      [RED, 0, RED, 0, WHITE],]
+        self.board = []
+        self.selected_piece = None
