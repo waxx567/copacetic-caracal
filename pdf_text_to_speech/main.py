2963124de7ddcd046d78102ed2a371ce67b00131
Wayne McRae <waynem567@gmail.com>
2024-12-25 09:47:37 +0200

    Update main.py

--- pdf_text_to_speech/main.py
+++ pdf_text_to_speech/main.py
@@ -4,9 +4,11 @@ import PyPDF2
 myFile = open('On-the-Origin-of-Species.pdf', 'rb')
 myReader = PyPDF2.PdfReader(myFile)
 numPages = len(myReader.pages)
-print("Number of pages in the PDF: ", numPages)
+# print("Number of pages in the PDF:", numPages)
 
 speaker = pyttsx3.init()
-
-speaker.say("Hello everyone, welcome to the create audiobook app")
+print("Reading page 3...")
+page = myReader.pages[2]
+text = page.extract_text()
+speaker.say(text)
 speaker.runAndWait()
