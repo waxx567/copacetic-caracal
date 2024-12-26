4ca7c5ff8abdf0c2976790ba641fd08e67008fbf
Wayne McRae <waynem567@gmail.com>
2024-12-26 13:34:26 +0200

    Update board.py

--- games/checkers_game/checkers/board.py
+++ games/checkers_game/checkers/board.py
@@ -16,6 +16,17 @@ class Board:
             for col in range(row % 2, ROWS, 2):
                 pygame.draw.rect(win, RED, (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
+    def move(self, piece, row, col):
+        self.board[piece.row][piece.col], self.board[row][col] = self.board[row][col], self.board[piece.row][piece.col]
+        piece.move(row, col)
+
+        if row == ROWS - 1 or row == 0:
+            piece.make_king()
+            if piece.color == WHITE:
+                self.white_kings += 1
+            else:
+                self.red_kings += 1
+
     def create_board(self):
         for row in range(ROWS):
             self.board.append([])
