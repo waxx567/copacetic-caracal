f83b38f9d40a61b6e861bbe2d11d1c8e7311604c
Wayne McRae <waynem567@gmail.com>
2024-12-26 12:10:42 +0200

    Update piece.py

--- games/checkers_game/checkers/piece.py
+++ games/checkers_game/checkers/piece.py
@@ -10,7 +10,7 @@ class Piece:
         self.row = row
         self.col = col
         self.color = color
-        self.king = False
+        self.king = True
         
         if self.color == RED:
             self.direction = -1
@@ -32,6 +32,9 @@ class Piece:
         radius = SQUARE_SIZE // 2 - self.PADDING
         pygame.draw.circle(win, GREY, (self.x, self.y), radius + self.OUTLINE)
         pygame.draw.circle(win, self.color, (self.x, self.y), radius)
+
+        if self.king:
+            win.blit(CROWN, (self.x - CROWN.get_width() // 2, self.y - CROWN.get_height() // 2))
 
     def __repr__(self):
         return str(self.color)
