9dd891931c3f8e5e237ee7bc39d9dc0aa6aa7690
Wayne McRae <waynem567@gmail.com>
2024-12-27 15:29:34 +0200

    Update models.py

--- crm/leads/models.py
+++ crm/leads/models.py
@@ -4,3 +4,5 @@ class Lead(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+
+    phoned = models.BooleanField(default=False)
