ea28aafa6f88bf05cb77ff84f6040d5d34975fe1
Wayne McRae <waynem567@gmail.com>
2024-12-27 19:42:09 +0200

    Update settings.py

--- django_saas_app/src/djhome/settings.py
+++ django_saas_app/src/djhome/settings.py
@@ -56,6 +56,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [    
             BASE_DIR / 'templates',
+            BASE_DIR / 'djhome',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
