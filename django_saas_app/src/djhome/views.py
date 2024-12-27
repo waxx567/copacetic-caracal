e73032761639c514c65d27a25107c46614b4418f
Wayne McRae <waynem567@gmail.com>
2024-12-27 19:25:00 +0200

    Update views.py

--- django_saas_app/src/djhome/views.py
+++ django_saas_app/src/djhome/views.py
@@ -5,14 +5,17 @@ this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
     my_title = 'My Home Page'
-    html_ = f'''
+    my_context = {
+        'page_title': my_title
+    }
+    html_ = '''
 <!DOCTYPE html>
 <html>
 <body>
-    <h1>Welcome to {my_title}</h1>
+    <h1>Welcome to {page_title}</h1>
 </body>
 </html>
-'''
+'''.format(page_title=my_title)
     # html_file_path = this_dir / 'home.html'
     # html_ = html_file_path.read_text()
     return HttpResponse(html_)
