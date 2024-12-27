938c267c222ec639ca2a08a614c43d82599c09c0
Wayne McRae <waynem567@gmail.com>
2024-12-27 15:30:54 +0200

    Update models.py

--- crm/leads/models.py
+++ crm/leads/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class Lead(models.Model):
+
+    SOURCE_CHOICES = (
+        ('Phone', 'Phone'),
+        ('Website', 'Website'),
+        ('Google', 'Google'),
+        ('Facebook', 'Facebook'),
+    )
+    
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
 
     phoned = models.BooleanField(default=False)
+    source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
