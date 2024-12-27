39abaf15d92d25f40cc97e210894a23dd4fd9f57
Wayne McRae <waynem567@gmail.com>
2024-12-27 19:17:21 +0200

    Update views.py

--- django_saas_app/src/djhome/views.py
+++ django_saas_app/src/djhome/views.py
@@ -5,7 +5,17 @@ this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
     print(this_dir)
-    html_ = ''
-    html_file_path = this_dir / 'home.html'
-    html_ = html_file_path.read_text()
+    html_ = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h1>Welcome to the Django Home Page</h1>
+</body>
+</html>
+'''
+    # html_file_path = this_dir / 'home.html'
+    # html_ = html_file_path.read_text()
     return HttpResponse(html_)
