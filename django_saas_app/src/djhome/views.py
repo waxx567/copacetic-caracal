79e8f5103c714fd5d868effcabbbca7cd897a6ce
Wayne McRae <waynem567@gmail.com>
2024-12-27 18:36:10 +0200

    Update views.py

--- django_saas_app/src/djhome/views.py
+++ django_saas_app/src/djhome/views.py
@@ -1,4 +1,9 @@
+import pathlib
 from django.http import HttpResponse
 
+this_file = pathlib.Path(__file__).resolve().parent
+
+
 def home_page_view(request, *args, **kwargs):
+    print(this_file)
     return HttpResponse('<h1>Hello, Django!</h1>')
