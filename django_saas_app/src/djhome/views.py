83ecacee6adcc09bd1947292b18dd43ea6a926df
Wayne McRae <waynem567@gmail.com>
2024-12-27 18:40:47 +0200

    Update views.py

--- django_saas_app/src/djhome/views.py
+++ django_saas_app/src/djhome/views.py
@@ -1,9 +1,12 @@
 import pathlib
 from django.http import HttpResponse
 
-this_file = pathlib.Path(__file__).resolve().parent
+this_dir = pathlib.Path(__file__).resolve().parent
 
 
 def home_page_view(request, *args, **kwargs):
     print(this_file)
+    html_ = ''
+    html_file_path = this_dir / 'home.html'
+
     return HttpResponse('<h1>Hello, Django!</h1>')
