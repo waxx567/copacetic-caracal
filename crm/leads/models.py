e2be12c1e7447a98da39ec42a38fba745efd7190
Wayne McRae <waynem567@gmail.com>
2024-12-27 15:32:07 +0200

    Update models.py

--- crm/leads/models.py
+++ crm/leads/models.py
@@ -8,10 +8,11 @@ class Lead(models.Model):
         ('Google', 'Google'),
         ('Facebook', 'Facebook'),
     )
-    
+
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
 
     phoned = models.BooleanField(default=False)
     source = models.CharField(choices=SOURCE_CHOICES, max_length=100)
+    profile_picture = models.ImageField(blank=True, null=True)
