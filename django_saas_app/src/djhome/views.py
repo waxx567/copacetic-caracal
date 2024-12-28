2f396f5f0e276d36d8d512b3d32e2b5e2bc22219
Wayne McRae <waynem567@gmail.com>
2024-12-28 15:48:31 +0200

    Updated views to change order

--- django_saas_app/src/djhome/views.py
+++ django_saas_app/src/djhome/views.py
@@ -10,6 +10,7 @@ def home_page_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
     my_title = 'My Home Page'
+    html_template = 'home.html'
     my_context = {
         'page_title': my_title,
         'page_visit_count': page_qs.count(),
@@ -18,7 +19,6 @@ def home_page_view(request, *args, **kwargs):
     }
     path = request.path
     print('path:', path)
-    html_template = 'home.html'
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
 
