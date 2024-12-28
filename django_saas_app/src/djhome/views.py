eb9974f4eb4c0a40f0d44cd612408e57a79a6b00
Wayne McRae <waynem567@gmail.com>
2024-12-28 15:17:25 +0200

    Test

--- django_saas_app/src/djhome/views.py
+++ django_saas_app/src/djhome/views.py
@@ -16,7 +16,7 @@ def home_page_view(request, *args, **kwargs):
     path = request.path
     print('path:', path)
     html_template = 'home.html'
-    PageVisit.objects.create()
+    PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
 
 
