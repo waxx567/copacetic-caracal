a8aad850d55c0506a86944e9461d8e7c2f5c44b8
Wayne McRae <waynem567@gmail.com>
2024-12-28 07:35:17 +0200

    Update models.py

--- django_saas_app/src/visits/models.py
+++ django_saas_app/src/visits/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class PageVisit(models.Model):
     # db -> table -> columns
+    # id -> primary key (autofield)
     path = models.TextField() # column
     timestamp = models.DateTimeField(auto_now_add=True) # column
