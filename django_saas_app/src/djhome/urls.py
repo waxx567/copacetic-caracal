2bd61abe511637c4353cbd17866dc4321850f5da
Wayne McRae <waynem567@gmail.com>
2024-12-29 07:21:33 +0200

    Change home_view() to point to about_view()

--- django_saas_app/src/djhome/urls.py
+++ django_saas_app/src/djhome/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_page_view
+from .views import home_view
 
 urlpatterns = [
-    path('', home_page_view), # index page -> root URL
-    path('hello-django/', home_page_view),
+    path('', home_view), # index page -> root URL
+    path('about/', home_view),
+    path('hello/', home_view),
+    path('hello-django/', home_view),
     path('admin/', admin.site.urls),
 ]
--- django_saas_app/src/djhome/views.py
+++ django_saas_app/src/djhome/views.py
@@ -7,21 +7,8 @@ from visits.models import PageVisit
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def home_page_view(request, *args, **kwargs):
-    qs = PageVisit.objects.all()
-    page_qs = PageVisit.objects.filter(path=request.path)
-    my_title = 'My Home Page'
-    html_template = 'home.html'
-    my_context = {
-        'page_title': my_title,
-        'page_visit_count': page_qs.count(),
-        'percent': (page_qs.count() * 100) / qs.count(),
-        'total_visit_count': qs.count()
-    }
-    path = request.path
-    print('path:', path)
-    PageVisit.objects.create(path=request.path)
-    return render(request, html_template, my_context)
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
 
 
 def about_view(request, *args, **kwargs):
