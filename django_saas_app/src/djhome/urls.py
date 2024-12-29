c6dbf2f3517a9a1d4ed6aca9d81610111968c5ac
Wayne McRae <waynem567@gmail.com>
2024-12-29 07:41:40 +0200

    Update urls.py

--- django_saas_app/src/djhome/urls.py
+++ django_saas_app/src/djhome/urls.py
@@ -17,12 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import home_view
+from .views import home_view, about_view
 
 urlpatterns = [
     path('', home_view), # index page -> root URL
     path('hello/', home_view),
-    path('about/', home_view),
+    path('about/', about_view),
     path('hello-django/', home_view),
     path('admin/', admin.site.urls),
 ]
