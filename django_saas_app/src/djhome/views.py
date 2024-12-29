0a7a9ee1897617429edf38e8452eee2ceabf0fc5
Wayne McRae <waynem567@gmail.com>
2024-12-29 06:27:13 +0200

    Day Startup

--- django_saas_app/src/djhome/views.py
+++ django_saas_app/src/djhome/views.py
@@ -6,6 +6,7 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
+
 def home_page_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
