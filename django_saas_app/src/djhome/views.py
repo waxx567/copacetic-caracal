43d923fd68a74798b7ab5e8a3d4e69914597aa7d
Wayne McRae <waynem567@gmail.com>
2024-12-29 07:16:25 +0200

    Change handling of percent in about_view()
    Handle ZeroDivisionError.

--- django_saas_app/src/djhome/views.py
+++ django_saas_app/src/djhome/views.py
@@ -27,12 +27,16 @@ def home_page_view(request, *args, **kwargs):
 def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100) / qs.count()
+    except ZeroDivisionError:
+        percent = 0
     my_title = 'My Page'
     html_template = 'home.html'
     my_context = {
         'page_title': my_title,
         'page_visit_count': page_qs.count(),
-        'percent': (page_qs.count() * 100) / qs.count(),
+        'percent': percent,
         'total_visit_count': qs.count()
     }
     return render(request, html_template, my_context)
