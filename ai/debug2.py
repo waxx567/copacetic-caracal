595db288eab378bc5227cfba3c90b9a619688fba
Wayne McRae <waynem567@gmail.com>
2025-04-07 13:23:01 +0200

    Create debug2.py

---
+++ ai/debug2.py
@@ -0,0 +1,3 @@
+'''
+You are developing an application to manage sensor data for IoT systems. Specifically, the sensor readings are stored in a binary search tree (BST) structure where each node contains a single unique timestamp as the node's key along with a sensor reading. You need to design functionality to retrieve all sensor readings in order that were recorded within a specific time range, while doing so, you have noticed that something is not working as expected.
+'''
