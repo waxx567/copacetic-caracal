1a6e99feb093534f745259146aa5aed1d371154d
Wayne McRae <waynem567@gmail.com>
2025-04-07 13:12:48 +0200

    Create debug1.py

---
+++ ai/debug1.py
@@ -0,0 +1,4 @@
+'''
+Your colleague has written a simple statistical analysis function in Python to calculate the number of occurrences of each item in a dataset. The function is expected to perform efficiently, ideally with O(n) time complexity. However, something went wrong, leading to unintended poor performance (far worse than linear complexity).
+'''
+def calculate_occurrences(items):
